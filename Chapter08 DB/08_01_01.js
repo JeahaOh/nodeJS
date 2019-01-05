f4d4e00549300fa87f8fb4ad090e8d6cdeafa2eb
JeahaOh <jeaha1217@gmail.com>
2019-01-06 02:00:39 +0900

    nodeJs Studies

---
+++ Chapter08 DB/08_01_01.js
@@ -0,0 +1,22 @@
+const mysql         = require('mysql');
+const connection    = mysql.createConnection({
+    host    :   'localhost',
+    user    :   'userid',
+    password:   '1111',
+    database:   'nodeDB'
+});
+
+connection.connect( (err) => {
+    if (err) {
+        console.log('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+    if (err) throw err;
+    console.log('The solution is : ', rows[0].solution);
+});
+
+connection.end();
