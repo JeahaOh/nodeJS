9c0c23c3c491201cbd51e575c2e54553b601ac83
JeahaOh <jeaha1217@gmail.com>
2019-01-19 22:26:35 +0900

    .

---
+++ Chapter08 DB/08_02_02.js
@@ -0,0 +1,14 @@
+const redis = require('redis'),
+    client = redis.createClient( 6379 , '10.10.10.10' );
+    client.auth( "password" );
+
+client.on( "subscribe", (channel, message) => {
+    console.log("client subscribe channel " + channel );
+});
+
+client.on( "message", (channel, message) => {
+    console.log("client message channel " + chennel + ": " + message);
+});
+
+client.subscribe( "pushnoti" );
+
