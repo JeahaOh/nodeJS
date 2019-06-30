2c428b197d406d89adfe2d606da2cca96140bd89
JeahaOh <jeaha1217@gmail.com>
2019-06-30 13:41:20 +0900

    22 JavaScript - 함수의 기본 문법

---
+++ opentutorialsNodeJS/syntax/function.js
@@ -0,0 +1,13 @@
+f123();
+console.log( 'asdf' );
+f123();
+console.log( 'FC' );
+f123();
+console.log( 'UAZ' );
+f123();
+
+function f123() {
+    console.log( 1 );
+    console.log( 2 );
+    console.log( 3 );
+}
