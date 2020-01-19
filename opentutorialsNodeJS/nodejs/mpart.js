36fb8c79f05928f79e628d964b35a52c22c2b3e8
JeahaOh <jeaha1217@gmail.com>
2020-01-19 11:48:50 +0900

    42 NodeJS - 모듈의 형식

---
+++ opentutorialsNodeJS/nodejs/mpart.js
@@ -0,0 +1,9 @@
+//  Module Part
+const M = {
+    v : 'V',
+    f : function() {
+        console.log( this.v );
+    }
+}
+
+module.exports = M;
---
+++ opentutorialsNodeJS/nodejs/muse.js
@@ -0,0 +1,15 @@
+//  Module Use
+/*
+const M = {
+    v : 'V',
+    f : function() {
+        console.log( this.v );
+    }
+}
+
+M.f();
+*/
+
+const part = require( './mpart.js' );
+console.log( part );
+part.f();
